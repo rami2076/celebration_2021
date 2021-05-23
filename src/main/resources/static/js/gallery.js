84f55baedb0c7847cb5a411a6045fe4f9a7e8df3
nyx <sakusennx@gmail.com>
2021-05-23 18:03:39 +0900

    Fix:Initialize Slick.

--- src/main/resources/static/js/gallery.js
+++ src/main/resources/static/js/gallery.js
@@ -1,10 +1,62 @@
 window.onload = function () {
-    $('.variants-container').slick({
+    // $('.variants-container').slick({
+    //     infinite: true,
+    //     dots: true,
+    //     // centerMode: true,
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1,
+    //     initialSlide: 0
+    // });
+
+
+    // $('.slider-for').slick({
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1,
+    //     arrows: false,
+    //     fade: true,
+    //     asNavFor: '.slider-nav'
+    // });
+    // $('.slider-nav').slick({
+    //     slidesToShow: 3,
+    //     slidesToScroll: 1,
+    //     asNavFor: '.slider-for',
+    //     dots: true,
+    //     centerMode: true,
+    //     focusOnSelect: true
+    // });
+     $(".variants-container").slick({
+
+        // normal options...
         infinite: true,
         dots: true,
-        // centerMode: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        initialSlide: 0
+        // autoplay: true,
+
+        // the magic
+        responsive: [{
+
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+            }
+
+        }, {
+
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                dots: true
+            }
+
+        }, {
+
+            breakpoint: 300,
+            settings: "unslick" // destroys slick
+
+        }]
     });
+
+
 };
+const slickInit = () => {
+    $('.variants-container').slick('setPosition');
+}
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -6,6 +6,7 @@ function seek() {
 function hide() {
     light()
     galleryView()
+    slickInit()
 }
 
 
